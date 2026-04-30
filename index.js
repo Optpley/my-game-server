0bfbb442df4cd71cecccceff282606de3699a151
Optpley <nixpley2222@gmail.com>
2026-04-30 22:32:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-import mongoose from "mongoose";
 
-mongoose.connect(process.env.MONGO_URL)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error("MongoDB error:", err));
-import express from "express";
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Server is running");
-});
-
-app.listen(3000, () => console.log("Server started on port 3000"));
