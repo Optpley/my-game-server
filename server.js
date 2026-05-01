cd154c42ef8a89f110c5c4552914d1b0b86c70e0
Optpley <nixpley2222@gmail.com>
2026-05-01 14:20:46 +0300

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+app.use(express.json());
+
+// Подключение роутов игроков
+const playerRoutes = require("./routes/player");
+app.use("/player", playerRoutes);
+
+// 🔥 Подключение роутов Ice Arena
+const gameRoutes = require("./routes/game");
+app.use("/game", gameRoutes);
+
+// Запуск сервера
+app.listen(3000, () => console.log("Server started"));
 
