9b9ddafa205ef73768a137e76d46a663e65a9b0f
Optpley <nixpley2222@gmail.com>
2026-05-01 19:51:30 +0300

    Update app.js

--- client/app.js
+++ client/app.js
@@ -1,1 +1,11 @@
+const tg = window.Telegram.WebApp;
+tg.expand();
+
+document.getElementById("load").onclick = async () => {
+    const res = await fetch("http://localhost:3000/game/status");
+    const data = await res.json();
+
+    document.getElementById("output").textContent =
+        JSON.stringify(data, null, 2);
+};
 
